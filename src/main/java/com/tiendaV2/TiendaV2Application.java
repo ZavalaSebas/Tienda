e526312e78918d1a1f7a8fda17adc0266c5e26aa
Sebas Zavala <sebaszavala120@gmail.com>
2023-06-01 22:58:17 -0600

    Segunda Version
    Version mejor trabajada

---
+++ src/main/java/com/tiendaV2/TiendaV2Application.java
@@ -0,0 +1,13 @@
+package com.tiendaV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaV2/TiendaV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
