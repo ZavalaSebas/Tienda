12f1dc71a76b6d5345a957353723eb4fedd69331
Sebas Zavala <sebaszavala120@gmail.com>
2023-08-10 11:11:08 -0600

    Version Final Proyecto Tienda
    Version Final :p

---
+++ src/main/java/com/tienda_v1/SecurityConfig.java
@@ -0,0 +1,88 @@
+package com.tienda_v1;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+    // Se definen los usuarios del sistema en formato de memoria
+//    @Bean
+//    public UserDetailsService users() {
+//        UserDetails admin = User.builder().username("juan")
+//                .password("{noop}123")
+//                .roles("USER", "VENDEDOR", "ADMIN")
+//                .build();
+//        
+//        UserDetails vendedor = User.builder().username("rebeca")
+//                .password("{noop}456")
+//                .roles("USER", "VENDEDOR")
+//                .build();
+//        
+//        UserDetails usuario = User.builder().username("pedro")
+//                .password("{noop}789").roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(usuario, vendedor, admin);
+//    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/",
+                        "/index",
+                        "/errores/**",
+                        "/carrito/**",
+                          "/reportes/**",
+                        "/webjars/**").permitAll()
+                .requestMatchers(
+                        "/articulo/nuevo",
+                        "/articulo/guardar",
+                        "/articulo/modificar/**",
+                        "/articulo/eliminar/**",
+                        "/categoria/nuevo",
+                        "/categoria/guardar",
+                        "/categoria/modificar/**",
+                        "/categoria/eliminar/**",
+                        "/cliente/nuevo",
+                        "/cliente/guardar",
+                        "/cliente/modificar/**",
+                        "/cliente/eliminar/**",
+                         "/reportes/**"
+                ).hasRole("ADMIN")
+                .requestMatchers(
+                        "/articulo/listado",
+                        "/categoria/listado",
+                        "/cliente/listado"
+                ).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers("/facturar/carrito")
+                .hasRole("USER")
+                )
+                .formLogin((form) -> form
+                .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll())
+                .exceptionHandling()
+                .accessDeniedPage("/errores/403");
+        return http.build();
+    }
+}
---
+++ src/main/java/com/tienda_v1/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,42 @@
+package com.tienda_v1.service.impl;
+
+import com.tienda_v1.dao.ClienteDao;
+import com.tienda_v1.dao.CreditoDao;
+import com.tienda_v1.domain.Cliente;
+import com.tienda_v1.domain.Credito;
+import com.tienda_v1.service.ClienteService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+// Esto es lo que la convierte en un servicio
+@Service
+public class ClienteServiceImpl implements ClienteService{
+    // Esto crea una unica copia de un objeto
+    @Autowired
+    private ClienteDao clienteDao;
+    @Autowired
+    private CreditoDao creditoDao;
+    @Override
+    public List<Cliente> getClientes() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    public Cliente getCliente(Cliente cliente) {
+        return clienteDao.findById(cliente.getIdCliente()).orElse(null);
+    }
+
+    @Override
+    public void deleteCliente(Cliente cliente) {
+        clienteDao.delete(cliente);
+    }
+
+    @Override
+    public void saveCliente(Cliente cliente) {
+        Credito credito = cliente.getCredito();
+        credito = creditoDao.save(credito);
+        cliente.setCredito(credito);
+        clienteDao.save(cliente);
+    }
+    
+}
